fcc022cc4adb73c92e8b2c9767d3eb4764203e68
pancasutresna <panca@sutresna.com>
2016-01-21 17:37:53 +0700

    Implementing Serverside Authorization

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,15 @@
 var auth = require('./auth');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 module.exports = function(app){
+
+     app.get('/api/users', auth.requiresRole('admin'), function(req, res){
+         User.find({}).exec(function(err, collection){
+             res.send(collection);
+         });
+     });
+
     // Angular partials setup
     app.get('/partials/*', function(req, res){
         res.render('partials/' + req.params[0]);
