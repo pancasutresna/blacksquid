cc48040ff543860cb9aed9680c45497de997b6a0
pancasutresna <panca@sutresna.com>
2016-01-25 10:04:00 +0700

    change setting on karma.conf.js to enable Continues integration mode

--- server/controller/placeCtrl.js
+++ server/controller/placeCtrl.js
@@ -2,7 +2,6 @@ var Place = require('mongoose').model('Place');
 
 exports.getPlaces = function(req, res){
     Place.find({}).exec(function(err, collection){
-        console.log(collection);
         res.send(collection);
     });
 };
