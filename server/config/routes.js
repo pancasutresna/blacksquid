add2e49ed7e44035abff154657b409b9dc66b0ed
pancasutresna <panca@sutresna.com>
2016-01-26 18:48:14 +0700

    add comment

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,6 +7,7 @@ var User = mongoose.model('User');
 module.exports = function(app){
 
      app.get('/api/users', auth.requiresRole('admin'), userCtrl.getUsers);
+     // test comment
      app.post('/api/users', userCtrl.createUser);
      app.put('/api/users', userCtrl.updateUser);
 
