40e3ecb16d0ff9638820f2fba3d75b925d7efe11
pancasutresna <panca@sutresna.com>
2016-03-21 18:00:13 +0700

    editing comment

--- client/app/data/data.module.js
+++ client/app/data/data.module.js
@@ -4,10 +4,10 @@
     angular.module('app.data', [
 
         // Also uses these, but we get them from core:
-//        'common',
-//        'blocks.exception', 'blocks.logger',
-//        'breeze.angular',   // tells breeze to use $q instead of Q.js
-//        'breeze.directives',// breeze validation directive (zValidate)
-//        'ngzWip'            // zStorage and zStorageWip
+        //        'common',
+        //        'blocks.exception', 'blocks.logger',
+        //        'breeze.angular',   // tells breeze to use $q instead of Q.js
+        //        'breeze.directives',// breeze validation directive (zValidate)
+        //        'ngzWip'            // zStorage and zStorageWip
     ]);
 })();
